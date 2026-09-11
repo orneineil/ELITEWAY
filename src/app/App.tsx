efefbce76d2e19a440ed6cdd6866be6a1788e181
orneineil <neillyorneilla26@gmail.com>
2026-09-11 02:59:52 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { useEffect } from "react";
+import { ClientAuthProvider } from "./contexts/ClientAuthContext";
 
 export default function App() {
   useEffect(() => {
@@ -14,5 +15,9 @@ export default function App() {
     }
   }, []);
 
-  return <RouterProvider router={router} />;
+  return (
+    <ClientAuthProvider>
+      <RouterProvider router={router} />
+    </ClientAuthProvider>
+  );
 }
